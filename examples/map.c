6b6cac1f559ac323c3600d319c0666b67d7e45b5
Xie Benyi <xieby1@outlook.com>
2021-07-08 14:26:52 +0800

    format the code by clang-format with customed config

--- examples/map.c
+++ examples/map.c
@@ -1,32 +1,36 @@
 #include "../pmparser.h"
 
 /**
-* 
-* Usage: map [pid]
-*/
+ *
+ * Usage: map [pid]
+ */
 
-int main(int argc, char* argv[]){
-	int pid;
-	if(argc==2) pid=atoi(argv[1]);
-	else pid=-1;
+int main(int argc, char *argv[])
+{
+    int pid;
+    if (argc == 2)
+        pid = atoi(argv[1]);
+    else
+        pid = -1;
 
-	procmaps_iterator* maps = pmparser_parse(pid);
-	if(maps==NULL){
-		printf ("[map]: cannot parse the memory map of %d\n",pid);
-		return -1;
-	}
+    procmaps_iterator *maps = pmparser_parse(pid);
+    if (maps == NULL)
+    {
+        printf("[map]: cannot parse the memory map of %d\n", pid);
+        return -1;
+    }
 
-	//iterate over areas
-	procmaps_struct* maps_tmp=NULL;
-	
-	while( (maps_tmp = pmparser_next(maps)) != NULL){
-		pmparser_print(maps_tmp,0);
-		printf("\n~~~~~~~~~~~~~~~~~~~~~~~~~\n"); 
-	}
+    // iterate over areas
+    procmaps_struct *maps_tmp = NULL;
 
-	//mandatory: should free the list
-	pmparser_free(maps);
+    while ((maps_tmp = pmparser_next(maps)) != NULL)
+    {
+        pmparser_print(maps_tmp, 0);
+        printf("\n~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+    }
 
-	return 0;
+    // mandatory: should free the list
+    pmparser_free(maps);
 
+    return 0;
 }
--- pmparser.h
+++ pmparser.h
@@ -14,56 +14,59 @@ implied warranty.
 
 #ifndef H_PMPARSER
 #define H_PMPARSER
+#include <errno.h>
+#include <fcntl.h>
+#include <linux/limits.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
-#include <sys/types.h>
 #include <sys/stat.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <linux/limits.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-//maximum line length in a procmaps file
-#define PROCMAPS_LINE_MAX_LENGTH  (PATH_MAX + 100) 
+// maximum line length in a procmaps file
+#define PROCMAPS_LINE_MAX_LENGTH (PATH_MAX + 100)
 /**
  * procmaps_struct
  * @desc hold all the information about an area in the process's  VM
  */
-typedef struct procmaps_struct{
-	void* addr_start; 	//< start address of the area
-	void* addr_end; 	//< end address
-	unsigned long length; //< size of the range
+typedef struct procmaps_struct
+{
+    void *addr_start;     //< start address of the area
+    void *addr_end;       //< end address
+    unsigned long length; //< size of the range
 
-	char perm[5];		//< permissions rwxp
-	short is_r;			//< rewrote of perm with short flags
-	short is_w;
-	short is_x;
-	short is_p;
+    char perm[5]; //< permissions rwxp
+    short is_r;   //< rewrote of perm with short flags
+    short is_w;
+    short is_x;
+    short is_p;
 
-	long offset;	//< offset
-	char dev[12];	//< dev major:minor
-	int inode;		//< inode of the file that backs the area
+    long offset;  //< offset
+    char dev[12]; //< dev major:minor
+    int inode;    //< inode of the file that backs the area
 
-	char pathname[600];		//< the path of the file that backs the area
-	//chained list
-	struct procmaps_struct* next;		//<handler of the chinaed list
+    char pathname[600]; //< the path of the file that backs the area
+    // chained list
+    struct procmaps_struct *next; //<handler of the chinaed list
 } procmaps_struct;
 
 /**
  * procmaps_iterator
  * @desc holds iterating information
  */
-typedef struct procmaps_iterator{
-	procmaps_struct* head;
-	procmaps_struct* current;
+typedef struct procmaps_iterator
+{
+    procmaps_struct *head;
+    procmaps_struct *current;
 } procmaps_iterator;
 /**
  * pmparser_parse
- * @param pid the process id whose memory map to be parser. the current process if pid<0
+ * @param pid the process id whose memory map to be parser. the current process
+ * if pid<0
  * @return an iterator over all the nodes
  */
-procmaps_iterator* pmparser_parse(int pid);
+procmaps_iterator *pmparser_parse(int pid);
 
 /**
  * pmparser_next
@@ -71,29 +74,27 @@ procmaps_iterator* pmparser_parse(int pid);
  * @param p_procmaps_it the iterator to move on step in the chained list
  * @return a procmaps structure filled with information about this VM area
  */
-procmaps_struct* pmparser_next(procmaps_iterator* p_procmaps_it);
+procmaps_struct *pmparser_next(procmaps_iterator *p_procmaps_it);
 /**
  * pmparser_free
  * @description should be called at the end to free the resources
  * @param p_procmaps_it the iterator structure returned by pmparser_parse
  */
-void pmparser_free(procmaps_iterator* p_procmaps_it);
+void pmparser_free(procmaps_iterator *p_procmaps_it);
 
 /**
  * _pmparser_split_line
  * @description internal usage
  */
-void _pmparser_split_line(char*buf,char*addr1,char*addr2,char*perm, char* offset, char* device,char*inode,char* pathname);
+void _pmparser_split_line(char *buf, char *addr1, char *addr2, char *perm,
+                          char *offset, char *device, char *inode,
+                          char *pathname);
 
 /**
  * pmparser_print
  * @param map the head of the list
  * @order the order of the area to print, -1 to print everything
  */
-void pmparser_print(procmaps_struct* map,int order);
-
-
-
-
+void pmparser_print(procmaps_struct *map, int order);
 
 #endif
